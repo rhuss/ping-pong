559d474234dcf60b73a05558d89707f7f30b5781
Roland Huß <roland@ro14nd.de>
2016-11-01 14:58:26 +0100

    Updated version

--- cluster_build.sh
+++ cluster_build.sh
@@ -5,5 +5,7 @@ do
   echo "================== Building on n${i} ========================"
   export DOCKER_HOST=tcp://n${i}:2375
   unset DOCKER_TLS_VERIFY
+  cd ping
   mvn -Ddmp.from="hypriot/rpi-java" clean install docker:build
+  cd ..
 done
