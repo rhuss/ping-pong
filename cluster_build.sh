6012d0494aec24bfaa9ad9dea884522dcee63a6f
Roland Huß <roland@ro14nd.de>
2018-04-09 21:13:54 +0200

    fix: add cluster_build.sh

---
+++ cluster_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in 0 1 2 3
+do
+  echo "================== Building on n${i} ========================"
+  export DOCKER_HOST=tcp://n${i}:2375
+  unset DOCKER_TLS_VERIFY
+  mvn -Ddmp.from="hypriot/rpi-java" clean install docker:build
+done
